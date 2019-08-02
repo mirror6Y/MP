0bc7994856f93ceb4b8320eded1e1bbaca51cbef
刘野 <liuye@thunisoft.com>
2019-08-02 18:45:03 +0800

    启动异常

---
+++ src/main/java/com/mybatis/plus/demo/Constants.java
@@ -0,0 +1,24 @@
+package com.mybatis.plus.demo;
+
+/**
+ * @author mirror6
+ * @description
+ * @date 2019/8/2 16:59
+ */
+public class Constants {
+
+    /**
+     * 操作成功
+     */
+    public static final String C_SUCCESS = "success";
+
+    /**
+     * 操作失败
+     */
+    public static final String C_FAIL = "fail";
+
+    /**
+     * 对数据进行CURD成功时返回值为1
+     */
+    public static final String C_CURD_SUCCESS = "1";
+}
--- src/main/java/com/mybatis/plus/demo/controller/TeacherController.java
+++ src/main/java/com/mybatis/plus/demo/controller/TeacherController.java
@@ -1,21 +1,50 @@
 package com.mybatis.plus.demo.controller;
 
 
+import com.mybatis.plus.demo.Constants;
+import com.mybatis.plus.demo.entity.Teacher;
+import com.mybatis.plus.demo.service.ITeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
- * @author mirror6123
+ * @author mirror6
  * @since 2019-07-31
  */
-@Controller
-@RequestMapping("/mybatis.plus.demo/teacher")
+@RestController
+@RequestMapping("/teacher")
 public class TeacherController {
+
+    private final ITeacherService teacherService;
+
+    /**
+     * 构造器注入teacherService
+     */
+    @Autowired
+    public TeacherController(ITeacherService teacherService) {
+        this.teacherService = teacherService;
+    }
+
+
+    /**
+     * 添加教师信息
+     * url:"http://localhost:8088/teacher/addTeacher"
+     */
+    @PostMapping("/addTeacher")
+    public String addTeacher() {
+        Teacher teacher=new Teacher();
+        teacher.setCId("123");
+        teacherService.insert(teacher);
+        return Constants.C_SUCCESS;
+    }
 
 }
 
