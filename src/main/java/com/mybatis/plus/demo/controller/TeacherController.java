07388e0c27333b22bb262e2128ed4dac1709790e
刘野 <liuye@thunisoft.com>
2019-08-26 17:36:34 +0800

    AOP全局日志

--- src/main/java/com/mybatis/plus/demo/controller/TeacherController.java
+++ src/main/java/com/mybatis/plus/demo/controller/TeacherController.java
@@ -1,20 +1,15 @@
 package com.mybatis.plus.demo.controller;
 
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.mybatis.plus.demo.util.Constants;
 import com.mybatis.plus.demo.entity.Teacher;
 import com.mybatis.plus.demo.service.ITeacherService;
+import com.mybatis.plus.demo.util.ResponseFactory;
+import com.mybatis.plus.demo.util.Result;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * <p>
@@ -39,25 +34,43 @@ public class TeacherController {
     }
 
 
-    /**
-     * url:"http://localhost:8088/teacher/addTeacher"
-     */
     @ApiOperation(value = "添加教师信息", notes = "添加", httpMethod = "POST")
-    @PostMapping("/addTeacher")
-    public String addTeacher() {
+    @PostMapping("addTeacher")
+    public Result addTeacher() {
         Teacher teacher = new Teacher();
         teacher.setCId("123");
-        teacherService.save(teacher);
-        return Constants.C_SUCCESS;
+        return ResponseFactory.build(teacherService.save(teacher));
     }
 
-///{pageNo}/{size}  @PathVariable Integer pageNo, @PathVariable Integer size
-//    @ApiOperation(value = "获得教师信息列表", notes = "列表信息", httpMethod = "GET")
-//    @GetMapping("getTeachers")
-//    public IPage<Teacher> getTeachers() {
+    @ApiOperation(value = "删除教师信息", notes = "删除", httpMethod = "DELETE")
+    @DeleteMapping("deleteTeacherById/{id}")
+    public Result deleteTeacherById(@PathVariable Integer id) {
+        return ResponseFactory.build(teacherService.removeById(id));
+    }
+
+    @ApiOperation(value = "编辑教师信息", notes = "修改", httpMethod = "PUT")
+    @PutMapping("updateTeacher")
+    public Result updateTeacher() {
+        Teacher teacher = new Teacher();
+        teacher.setCId("111");
+        teacher.setCName("111");
+        return ResponseFactory.build(teacherService.updateById(teacher));
+    }
+
+    @ApiOperation(value = "查询教师信息", notes = "查找", httpMethod = "GET")
+    @GetMapping("getTeacherById/{id}")
+    public Result getTeacherById(@PathVariable Integer id) {
+        return ResponseFactory.build(teacherService.getById(id));
+    }
+
+
+    ///{pageNo}/{size}  @PathVariable Integer pageNo, @PathVariable Integer size
+    @ApiOperation(value = "获得教师信息列表", notes = "列表信息", httpMethod = "GET")
+    @GetMapping("getTeachers")
+    public List<Teacher> getTeachers() {
 //        Page<Teacher> page = new Page<Teacher>(1, 5);
-//        return teacherService.page(page);
-//    }
+        return teacherService.list();
+    }
 
 }
 
---
+++ src/main/java/com/mybatis/plus/demo/util/Result.java
@@ -0,0 +1,30 @@
+package com.mybatis.plus.demo.util;
+
+import lombok.Data;
+
+/**
+ * @author mirror6
+ * @description service层开始返回Result
+ * @date 2019/8/26 11:19
+ */
+@Data
+public class Result {
+
+    /**
+     * 响应码
+     */
+    private int code;
+
+    /**
+     * 响应信息
+     */
+    private String msg;
+
+    /**
+     * 需要返回的数据对象
+     */
+    private Object data;
+
+    private int count = 0;
+
+}
